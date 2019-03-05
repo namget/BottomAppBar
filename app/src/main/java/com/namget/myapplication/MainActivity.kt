34680b0412ef66b1a9166adc260bcf0b485eec40
leejaewoo <yeboykr@neighbor21.co.kr>
2019-03-05 12:03:31 +0900

    - move FAB 추가

--- app/src/main/java/com/namget/myapplication/MainActivity.kt
+++ app/src/main/java/com/namget/myapplication/MainActivity.kt
@@ -2,11 +2,28 @@ package com.namget.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomappbar.BottomAppBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        moveFAB.setOnClickListener {
+            moveAnimation()
+        }
     }
+
+    //fab animation from center to end
+    fun moveAnimation() {
+        if (bar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_CENTER) {
+            bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_END
+        }else if(bar.fabAlignmentMode == BottomAppBar.FAB_ALIGNMENT_MODE_END){
+            bar.fabAlignmentMode = BottomAppBar.FAB_ALIGNMENT_MODE_CENTER
+        }
+
+    }
+
 }
